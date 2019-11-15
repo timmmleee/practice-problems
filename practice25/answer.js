6da998fcc466e369dd3128062acbf9670728873a
Timothy Lee <timothylee3508@gmail.com>
2019-11-15 13:09:48 -0800

    finished problem 25-done

--- practice25/answer.js
+++ practice25/answer.js
@@ -11,16 +11,12 @@ function addArrays (first_array, second_array) {
     }
 
     for (var num=0; num <= shortArr.length; num++) {
-      if(typeof(first_array[num]) == number && typeof(second_array[num])==number) { //fix this. what if its a string
+      if(typeof(first_array[num]) == 'number' && typeof(second_array[num]) == 'number') { //fix this. what if its a string
         var result = first_array[num] + second_array[num];
         output.push(result);
-      } else {
-        output.push('NaN');
       }
     }
     return output;
 }
 
 addArrays([1,2,'hi',3,4],[2,3,4,'bye',5,6]);
-
-//hello
