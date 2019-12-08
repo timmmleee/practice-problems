087267595cb1362c41143d8c9aafba6d482f2117
Timothy Lee <timothylee3508@gmail.com>
2019-12-08 13:12:36 -0800

    didn't finish

--- drills05/yourcode.js
+++ drills05/yourcode.js
@@ -1,7 +1,17 @@
+function populateRecords(array){
+  var newObject = {
+    'charge': null,
+    'cash advance': null
+  }
 
+  // for(var index = 0; index < array.length; index++) {
+  //   console.log(array[index].type)
 
-function populateRecords(){
-
+  //   if(array[index].type == 'charge'){
+  //     newObject['charge'] += array[index].amount;
+  //   } else if (array[index].type == 'cash advance'){
+  //     newObject['cash advance'] += array[index].amount
+  //   }
+  // }
+  // return newObject;
 }
-
-
