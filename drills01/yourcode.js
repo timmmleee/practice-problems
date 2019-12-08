5ecac66f0be550790ddace5cd892b35dc08c81d2
Timothy Lee <timothylee3508@gmail.com>
2019-12-07 17:16:17 -0800

    finished drill 1

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,44 @@
 
-function sumArray(){
-	
+function sumArray(array){
+  var total = 0;
+    for (var index = 0; index < array.length; index++) {
+        total += array[index];
+    }
+    return total;
 }
 
-function fitWithinVal(){
+function fitWithinVal(array,num){
+  var newArray = [];
+  var total = 0;
 
+  for (var index = 0; index < array.length; index++){
+    var currentNum = array[index];
+    total += currentNum;
+
+    if (total <= num) {
+      newArray.push(currentNum);
+    } else {
+      total -= currentNum;
+    }
+  }
+  return newArray;
 }
 
-function getAllNamesShorterThan(){
+function getAllNamesShorterThan(array,num){
+  var namesThatAreShorter = [];
 
+  for (var index = 0; index < array.length; index++) {
+    var currentWord = array[index];
+
+    if(currentWord.length < num) {
+      namesThatAreShorter.push(currentWord);
+    }
+  }
+  return namesThatAreShorter;
 }
 
-function makeLabel(){
-
+function makeLabel(someObject){
+  return `${someObject["greeting"]} ${someObject["givenName"]} ${someObject["familyName"]}
+${someObject["home address"].streetNumber} ${someObject["home address"].streetName}
+${someObject["home address"].city}, ${someObject["home address"].state} ${someObject["home address"].zip}`
 }
