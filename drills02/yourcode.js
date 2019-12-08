085ee7daa9bfef1ae8bd2d4af7fdcf009f4ffa73
Timothy Lee <timothylee3508@gmail.com>
2019-12-07 20:11:43 -0800

    did all but findMode

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,16 +1,44 @@
+function countOccurences(array,word){
+  var wordFound = 0;
 
-
-function countOccurences(){
-
+  for (var index = 0; index < array.length; index++) {
+    if (array[index] === word) {
+      wordFound++;
+    }
+  }
+  return wordFound;
 }
 
-function wordLengths(){
+function wordLengths(array){
+  var wordLengthArray = [];
 
+  for (var index = 0; index < array.length; index++) {
+   var wordLength =  array[index].length;
+   wordLengthArray.push(wordLength);
+  }
+  return wordLengthArray;
 }
 
-function getMinMaxMean(){
+function getMinMaxMean(array){
+  var minMaxMeanObject = {
+    "min": null,
+    "max": null,
+    "mean": null
+  }
+  var totalSum = 0;
+  var totalNum = 0;
 
+  for (var index = 0; index < array.length; index++) {
+    minMaxMeanObject["min"] = Math.min.apply(Math,array);
+    minMaxMeanObject["max"] = Math.max.apply(Math,array);
+    totalSum += array[index];
+    totalNum++;
+    var mean = parseInt(totalSum / totalNum);
+    minMaxMeanObject["mean"] = mean;
+  }
+  return minMaxMeanObject;
 }
+
 
 function findMode(){
 
